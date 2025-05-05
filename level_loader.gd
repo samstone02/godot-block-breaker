5113237e272f330c485d597194bbd07012da4f24
Sam Stone <samuelrystone@gmail.com>
2025-05-04 22:40:06 -0400

    fix bug when retrying same level more than 1 time in a row

--- level_loader.gd
+++ level_loader.gd
@@ -4,8 +4,6 @@ var loaded_level: String = ""
 var loaded_level_path: String = ""
 
 func _load_level(level_name: String) -> void:
-	print("Loading level: " + level_name)
-	
 	var level: Node = load("res://levels/" + level_name + ".tscn").instantiate()
 	get_tree().root.get_node("MenuUi").visible = false
 	get_tree().root.add_child(level)
@@ -14,7 +12,11 @@ func _load_level(level_name: String) -> void:
 	loaded_level_path = "res://levels/" + level_name + ".tscn"
 
 func reload_level() -> void:
-	print("reloading level: " + loaded_level)
-	get_tree().root.get_node(loaded_level).free()
+	get_tree().root.get_node(loaded_level).queue_free()
 	var level: Node = load(loaded_level_path).instantiate()
 	get_tree().root.add_child(level)
+	
+	# since the level still exists at this point, the new instance will have a generated
+	# name rather than the name of the level.
+	# reassign the name so that we can correctly find it on the next call to reload_level
+	loaded_level = level.name
